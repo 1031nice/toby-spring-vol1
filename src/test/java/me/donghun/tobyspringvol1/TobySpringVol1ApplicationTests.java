78c7039bdd22ac9b8c80e42b6c33bef4fb71c54e
Dong-Hun Gang <1031nice@gmail.com>
2020-07-13 09:54:22 +0900

    let's start toby spring 3.1 vol1

---
+++ src/test/java/me/donghun/tobyspringvol1/TobySpringVol1ApplicationTests.java
@@ -0,0 +1,13 @@
+package me.donghun.tobyspringvol1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TobySpringVol1ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
