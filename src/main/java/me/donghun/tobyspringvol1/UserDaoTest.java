9d170bf2c1f4bf61839eff9e1981c73532c58903
Dong-Hun Gang <1031nice@gmail.com>
2020-07-24 22:44:32 +0900

    UserDaoTest 테스트 코드에서 직접 결과를 확인하도록 수정

--- src/main/java/me/donghun/tobyspringvol1/UserDaoTest.java
+++ src/main/java/me/donghun/tobyspringvol1/UserDaoTest.java
@@ -26,19 +26,26 @@ public class UserDaoTest {
 //        Object userDao = context.getBean("userDao"); // 클래스를 안주면 Object 반환
         UserDao dao = new DaoFactory().userDao();
 
-//        User user = new User();
-//        user.setId("donghun");
-//        user.setName("강동훈");
-//        user.setPassword("1031");
-//
+        User user = new User();
+        user.setId("donghun");
+        user.setName("강동훈");
+        user.setPassword("1031");
+
 //        dao.add(user);
-//
+
 //        System.out.println(user.getId() + " 등록 성공");
 
         User user2 = dao.get("donghun");
-        System.out.println(user2.getName());
-        System.out.println(user2.getPassword());
-        System.out.println(user2.getId() + " 조회 성공");
+
+        if(!user.getName().equals(user2.getName())){
+            System.out.println("테스트 실패 (name)");
+        }
+        else if(!user.getPassword().equals(user2.getPassword())){
+            System.out.println("테스트 실패 (password)");
+        }
+        else{
+            System.out.println("조회 테스트 성공");
+        }
     }
 
 }
