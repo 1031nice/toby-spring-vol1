ce8b08d459910b459ba0b7b8cbcfced7aedf6441
Dong-Hun Gang <1031nice@gmail.com>
2020-08-18 21:25:44 +0900

    템플릿/콜백 패턴 적용 및 테스트 코드 개선

---
+++ src/main/java/me/donghun/tobyspringvol1/templateExam/BufferedReaderCallback.java
@@ -0,0 +1,10 @@
+package me.donghun.tobyspringvol1.templateExam;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+
+public interface BufferedReaderCallback {
+
+    Integer doSomethingWithReader(BufferedReader br) throws IOException;
+
+}
--- src/test/java/me/donghun/tobyspringvol1/templateExam/CalculatorTest.java
+++ src/test/java/me/donghun/tobyspringvol1/templateExam/CalculatorTest.java
@@ -1,5 +1,6 @@
 package me.donghun.tobyspringvol1.templateExam;
 
+import org.junit.Before;
 import org.junit.Test;
 
 import java.io.IOException;
@@ -9,11 +10,18 @@ import static org.assertj.core.api.Assertions.assertThat;
 
 public class CalculatorTest {
 
+    Calculator calculator;
+    String numFilepath;
+
+    @Before
+    public void setUp(){
+        this.calculator = new Calculator();
+        this.numFilepath = "C:\\Users\\1031n\\IntelliJ\\toby-spring-vol1\\src\\main\\resources\\numbers.txt";
+    }
+
     @Test
     public void sumOfNumbers() throws IOException {
-        Calculator calculator = new Calculator();
-        int sum = calculator.calSum("C:\\Users\\1031n\\IntelliJ\\toby-spring-vol1\\src\\main\\resources\\numbers.txt");
-        assertThat(sum).isEqualTo(10);
+        assertThat(calculator.calSum(this.numFilepath)).isEqualTo(10);
     }
 
 }
