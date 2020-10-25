5265c3f140190f22726d38834bf5660ef647212e
Dong-Hun Gang <1031nice@gmail.com>
2020-10-25 17:43:20 +0900

    스프링의 FactoryBean 학습 테스트 작성

---
+++ src/main/java/me/donghun/tobyspringvol1/factoryBean/Message.java
@@ -0,0 +1,18 @@
+package me.donghun.tobyspringvol1.factoryBean;
+
+public class Message {
+
+    String text;
+
+    private Message(String text){
+        this.text = text;
+    }
+
+    public String getText() {
+        return text;
+    }
+
+    public static Message newMessage(String text){
+        return new Message(text);
+    }
+}
---
+++ src/test/java/me/donghun/tobyspringvol1/factoryBean/FactoryBeanTest.java
@@ -0,0 +1,24 @@
+package me.donghun.tobyspringvol1.factoryBean;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.ApplicationContext;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration(locations = "/FactoryBeanTest-context.xml")
+public class FactoryBeanTest {
+    @Autowired
+    ApplicationContext context;
+
+    @Test
+    public void getMessageFromFactoryBean() {
+        Object message = context.getBean("message");
+        assertThat(message).isInstanceOf(Message.class);
+        assertThat(((Message)message).getText()).isEqualTo("Factory Bean");
+    }
+}
